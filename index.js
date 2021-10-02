c6912038a001f9401a418202dcf5577d97b92d43
MOHAMMAD RIZWANULLAH <78429736+rizwanrockzz@users.noreply.github.com>
2021-10-02 13:00:44 +0530

    Update index.js
    removed "/" before folders for github purposes

--- index.js
+++ index.js
@@ -3,42 +3,42 @@ var numberOfDrums = document.querySelectorAll(".drum").length;
 function makeSound(keyCodeForSound) {
     switch (keyCodeForSound) {
         case "w":
-            var tom1_audio = new Audio("/sounds/tom-1.mp3");
+            var tom1_audio = new Audio("sounds/tom-1.mp3");
             tom1_audio.play();
             break;
         case "a":
-            var tom2_audio = new Audio("/sounds/tom-2.mp3");
+            var tom2_audio = new Audio("sounds/tom-2.mp3");
             tom2_audio.play();
 
             break;
         case "s":
-            var tom3_audio = new Audio("/sounds/tom-3.mp3");
+            var tom3_audio = new Audio("sounds/tom-3.mp3");
             tom3_audio.play();
 
             break;
         case "d":
-            var tom4_audio = new Audio("/sounds/tom-4.mp3");
+            var tom4_audio = new Audio("sounds/tom-4.mp3");
             tom4_audio.play();
 
             break;
         case "j":
-            var crash_audio = new Audio("/sounds/crash.mp3");
+            var crash_audio = new Audio("sounds/crash.mp3");
             crash_audio.play();
 
             break;
         case "k":
-            var kickBass_audio = new Audio("/sounds/kick-bass.mp3");
+            var kickBass_audio = new Audio("sounds/kick-bass.mp3");
             kickBass_audio.play();
 
             break;
         case "l":
-            var snare_audio = new Audio("/sounds/snare.mp3");
+            var snare_audio = new Audio("sounds/snare.mp3");
             snare_audio.play();
 
             break;
 
         default:
-            var default_audio = new Audio("/sounds/sound-for-default.wav");
+            var default_audio = new Audio("sounds/sound-for-default.wav");
             default_audio.play();
             break;
     }
